09821ee61ef563eec24aa56f33d36e89d583dbd6
Joshua DiStefano <j.distefano9@gmail.com>
2018-02-25 23:31:07 -0500

    Update to v1.1

--- shaders/lib/framebuffer.glsl
+++ shaders/lib/framebuffer.glsl
@@ -16,4 +16,13 @@ const     int           gnormalFormat             = RGBA16;
 #define   FragData3    gl_FragData[3]
 #define   FragData4    gl_FragData[4]
 
+#define   RANDOM_ROTATION
+//#define   RANDOM_ROTATION_FILTER
+
+#define   PCF_SAMPLE_COUNT                          2      // [1 2 3 4 5]
+#define   PCSS_SAMPLE_COUNT                         3      // [1 2 3 4 5]
+#define   MIN_PENUMBRA_SIZE                         0.25   // [0.0 0.1 0.25 0.5]
+#define   LIGHT_SIZE                                75    // [50 75 100 125 150]
+#define   PCSS
+
 //#define   CLAY
